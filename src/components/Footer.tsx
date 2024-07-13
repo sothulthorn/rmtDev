c3dd2659f3acd9199cea7944098f0d30acb6f54c
sothulthorn <thornsothul@gmail.com>
2024-07-13 13:13:40 +0900

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          © Copyright by{' '}
+          <a href="https://github.com/sothulthorn" target="_blank">
+            Sothul Thorn
+          </a>
+          . Intended for learning or your portfolio.
+        </p>
+      </small>
+
+      <p>
+        <span className="u-bold">109573</span> total jobs available
+      </p>
+    </footer>
+  );
+}
