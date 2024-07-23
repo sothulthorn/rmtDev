27072a569d202a215f353a795b90a84d90b82a34
sothulthorn <thornsothul@gmail.com>
2024-07-23 22:14:08 +0900

    Close popover - useOnClickOutside

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,29 +1,15 @@
 import { TriangleDownIcon } from '@radix-ui/react-icons';
 import BookmarksPopover from './BookmarksPopover';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
+import { useOnClickOutside } from '../lib/hooks';
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
   const popOverRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
-      if (
-        e.target instanceof HTMLElement &&
-        !buttonRef.current?.contains(e.target) &&
-        !popOverRef.current?.contains(e.target)
-      ) {
-        setIsOpen(false);
-      }
-    };
-
-    document.addEventListener('click', handleClick);
-
-    return () => {
-      document.removeEventListener('click', handleClick);
-    };
-  }, []);
+  useOnClickOutside([buttonRef, popOverRef], () => {
+    setIsOpen(false);
+  });
 
   return (
     <section>
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -160,6 +160,26 @@ export const useLocalStorage = <T>(
 };
 
 // ----------------------------------------------------- //
+export const useOnClickOutside = (
+  refs: React.RefObject<HTMLElement>[],
+  handler: () => void
+) => {
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (refs.every((ref) => !ref.current?.contains(e.target as Node))) {
+        handler();
+      }
+    };
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, [refs, handler]);
+};
+
+// ----------------------------------------------------- //
 export const useBookmarksContext = () => {
   const context = useContext(BookmarkContext);
   if (!context) {
